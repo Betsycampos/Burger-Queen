9fb93a312df4a2a1643149ca6b88dbcf3c0b812e
Betsycampos <cbetsydalia@gmail.com>
2019-04-20 09:43:09 -0500

    Subiendo al repositorio la colleción de datos extraidos de firebase, ya se muestran en la consola satisfactoriamente

--- src/firebase-controller.js
+++ src/firebase-controller.js
@@ -1,1 +1,9 @@
-// 
+// export const dataMenu = (callback) =>
+//     firebase.firestore().collection('desayuno')
+//     .onSnapshot((querySnapshot) => {
+//         const arr = []; 
+//         querySnapshot.forEach((doc) => {
+//           arr.push({id: doc.id, ...doc.data()})
+//         });
+//         callback(arr)
+//     });
