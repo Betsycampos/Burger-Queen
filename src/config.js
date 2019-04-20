0752276960185c23b43cc78873d0f96f1673c6f4
Betsycampos <cbetsydalia@gmail.com>
2019-04-19 22:12:41 -0500

    Añadiendo modificaciones en el archivo config.js para hacer mis primeras pruebas de mostrar la data en consola

--- src/config.js
+++ src/config.js
@@ -1,14 +1,25 @@
-export const configFirebase = () => {   // Initialize Firebase
+// export const configFirebase = () => {   // Initialize Firebase
   const config = {
     apiKey: "AIzaSyAHafw898Kfjx6jaMx72vwK_UJhJXzdsuU",
     authDomain: "burger-queen-76a21.firebaseapp.com",
     databaseURL: "https://burger-queen-76a21.firebaseio.com",
     projectId: "burger-queen-76a21",
-    storageBucket: "burger-queen-76a21.appspot.com",
-    messagingSenderId: "695085114950"
+    // storageBucket: "burger-queen-76a21.appspot.com",
+    // messagingSenderId: "695085114950"
   };
   firebase.initializeApp(config);
-};
 
+
+
+  const db = firebase.firestore();
+db.settings({timestampsInSnapshots: true});
+
+db.collection('desayuno').get().then((snapshot) => {
+  console.log(snapshot.doc); 
+});
+// };
+
+// export const db = firebase.firestore();
+// db.settings({timestampsInSnapshots: true});
   
 
