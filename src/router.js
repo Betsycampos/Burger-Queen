e8c28633814c5feb72ea67546fec24926dcbaa6f
Betsycampos <cbetsydalia@gmail.com>
2019-04-20 09:44:51 -0500

    Subiendo al repositorio la colleción de datos extraidos de firebase, ya se muestran en la consola satisfactoriamente

--- src/router.js
+++ src/router.js
@@ -1,26 +1,26 @@
-import home from './UI/home.js'
-import {desayuno} from './UI/menu.js'
-export const templatePedido = (rutas) => {
-  const router = rutas.substr(2, rutas.length -2);
-  const pedido = document.getElementById('pedido');
-  pedido.innerHTML = '';
-  switch (router) {
-    case 'home':
-    pedido.appendChild(home());
-    break;
-    case 'desayuno':
-    pedido.appendChild(desayuno());
-    break;
-  };
-};
+// import home from './UI/home.js'
+// import {desayuno} from './UI/menu.js'
+// export const templatePedido = (rutas) => {
+//   const router = rutas.substr(2, rutas.length -2);
+//   const pedido = document.getElementById('pedido');
+//   pedido.innerHTML = '';
+//   switch (router) {
+//     case 'home':
+//     pedido.appendChild(home());
+//     break;
+//     case 'desayuno':
+//     pedido.appendChild(desayuno());
+//     break;
+//   };
+// };
 
-export const cambio = (hash) => {
-  if( hash === '#/home' || hash === '#/desayuno') return templatePedido(hash);
-  else return templatePedido('#/home');
-};
+// export const cambio = (hash) => {
+//   if( hash === '#/home' || hash === '#/desayuno') return templatePedido(hash);
+//   else return templatePedido('#/home');
+// };
 
-export const enrutador = () => {
-  window.addEventListener('load', cambio(window.location.hash))
-  if (('onhashchange' in window)) window.onhashchange = () => cambio((window.location.hash));
+// export const enrutador = () => {
+//   window.addEventListener('load', cambio(window.location.hash))
+//   if (('onhashchange' in window)) window.onhashchange = () => cambio((window.location.hash));
   
-} 
+// } 
