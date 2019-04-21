ad1c6598d734c1b83b6643fe9a629ea928235b74
Betsycampos <cbetsydalia@gmail.com>
2019-04-21 14:25:02 -0500

    Agregando funciones para que se muestre mi etiqueta section

--- src/UI/menu.js
+++ src/UI/menu.js
@@ -1,24 +1,24 @@
-// export const desayuno = () => {
-//     const formElement = document.createElement('form');
-//     const formPedido = `
-//     <div>
-//     <h2>Menú</h2>
-//     <input type="text" id="cliente">
-//     <button type="button"id="btn-home">Inicio</button>
-//     <div class="container">
-//     <div class="row">
-//     <div class="col-6">
-//     </div>
-//     </div>
-//     </div>
-//     </div>`;
-//     formElement.setAttribute('id', 'frm-home');
-//     formElement.innerHTML = formPedido;
-//     const btnRetorno = formElement.querySelector('#btn-home');
-//     const nombre = localStorage.getItem('nombre');
-//     formElement.querySelector('#cliente').value = nombre;
-//     btnRetorno.addEventListener('click', () => {
-//     window.location.hash = '#/home';
-//     });
-//     return formElement;
-//   };
+export const desayuno = () => {
+    const formElement = document.createElement('form');
+    const formPedido = `
+    <div>
+    <h2>Menú</h2>
+    <input type="text" id="cliente">
+    <button type="button"id="btn-home">Inicio</button>
+    <div class="container">
+    <div class="row">
+    <div class="col-6">
+    </div>
+    </div>
+    </div>
+    </div>`;
+    formElement.setAttribute('id', 'frm-home');
+    formElement.innerHTML = formPedido;
+    const btnRetorno = formElement.querySelector('#btn-home');
+    const nombre = localStorage.getItem('nombre');
+    formElement.querySelector('#cliente').value = nombre;
+    btnRetorno.addEventListener('click', () => {
+    window.location.hash = '#/home';
+    });
+    return formElement;
+  };
