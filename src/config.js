c402a8e37741a3fd4c3113b7eb6c0d9d896580c3
Betsycampos <cbetsydalia@gmail.com>
2019-04-22 15:51:01 -0500

    Añadiendo la colleción de almuerzo-cena a el archivo main.js para mostrar todos los datos en consola, ya listos para ser mostrados en la pantalla

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
   // Initialize Firebase
-  // export const configFirebase = () => {
+  export const configFirebase = () => {
   const config = {
     apiKey: "AIzaSyAHafw898Kfjx6jaMx72vwK_UJhJXzdsuU",
     authDomain: "burger-queen-76a21.firebaseapp.com",
@@ -9,8 +9,20 @@
     messagingSenderId: "695085114950"
   };
   firebase.initializeApp(config);
-// };
+};
 
+// const db = firebase.firestore();
+// db.settings({
+//   timestampsInSnapshots: true
+// }
+// );
+
+// db.collection("almuerzo-cena").get().then(function(querySnapshot) {
+//   querySnapshot.forEach(function(doc) {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, " => ", doc.data());
+//   });
+// });
 
 
 
