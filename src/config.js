d62a40a8dd01462c7266b33ec9b2222ad448d73f
Betsycampos <cbetsydalia@gmail.com>
2019-04-22 11:31:30 -0500

    Añadiendo la data en una función ya me muestra en consola pero es un ciclo infinito

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
   // Initialize Firebase
+  // export const configFirebase = () => {
   const config = {
     apiKey: "AIzaSyAHafw898Kfjx6jaMx72vwK_UJhJXzdsuU",
     authDomain: "burger-queen-76a21.firebaseapp.com",
@@ -8,20 +9,8 @@
     messagingSenderId: "695085114950"
   };
   firebase.initializeApp(config);
-  
+// };
 
-const db = firebase.firestore();
-db.settings({
-  timestampsInSnapshots: true
-}
-);
-
-db.collection("almuerzo-cena").get().then(function(querySnapshot) {
-  querySnapshot.forEach(function(doc) {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-  });
-});
 
 
 
