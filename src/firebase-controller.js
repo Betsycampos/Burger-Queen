49a145d29a92c4b57eed62a5a8d8010b181e312a
Betsycampos <cbetsydalia@gmail.com>
2019-04-22 11:31:51 -0500

    Añadiendo la data en una función ya me muestra en consola pero es un ciclo infinito

--- src/firebase-controller.js
+++ src/firebase-controller.js
@@ -1,9 +1,24 @@
-export const dataMenu = (callback) =>
-    firebase.firestore().collection('desayuno')
-    .onSnapshot((querySnapshot) => {
-        const arr = []; 
-        querySnapshot.forEach((doc) => {
-          arr.push({id: doc.id, ...doc.data()})
-        });
-        callback(arr)
+export const menuAlmuerzo = (callback) =>
+  firebase.firestore().collection("almuerzo-cena")
+  .onSnapshot((querySnapshot) => {
+    const arrAlmuerzo = [];
+    querySnapshot.forEach((doc) => {
+      arrAlmuerzo.push({id: doc.id, ...doc.data()});
     });
+    callback(arrAlmuerzo);
+  });
+  
+
+
+// const db = firebase.firestore();
+// db.settings({
+//   timestampsInSnapshots: true
+// }
+// );
+
+// db.collection("almuerzo-cena").get().then(function(querySnapshot) {
+//   querySnapshot.forEach(function(doc) {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, " => ", doc.data());
+//   });
+// });
